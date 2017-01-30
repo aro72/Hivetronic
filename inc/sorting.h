92d863700e04a52c10c6f937f0317fde19f5a04d
aro72 <aro_72@yahoo.fr>
2017-01-30 11:35:01 +0100

    Added merge and sort

---
+++ inc/sorting.h
@@ -0,0 +1,20 @@
+/*
+ * sorting.h
+ *
+ *  Created on: 30 janv. 2017
+ *      Author: Arnaud ROSAY
+ */
+
+#ifndef SORTING_H_
+#define SORTING_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// PROTOTYPES
+void merge (int *a, int n, int m);
+void merge_sort (int *a, int n);
+
+#endif /* SORTING_H_ */
